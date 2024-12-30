1e1b094f01826602f2a4ce85e676d9a82f021a09
Thenuri Gamage <thenugamage311@gmail.com>
2024-12-30 22:19:04 +0530

    adding hellow

--- testPOS/Form2.cs
+++ testPOS/Form2.cs
@@ -235,5 +235,15 @@ namespace testPOS
             loginForm.Show();
             this.Hide();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
